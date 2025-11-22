3d781fd8d5f58f201a3e4148a019b64678708453
clizzix <godsilla1993@gmail.com>
2025-11-22 11:39:55 +0100

    removed randomization of the Number, because its not pracitcal for a simple CLI Game

--- numberGuessingGame.js
+++ numberGuessingGame.js
@@ -1,3 +1,34 @@
-const args = process.argv.slice(2);
+// Generates a random integer between 1 and 100 (inclusive)
+const secretNumber = 46;
 
-const randomNumber = Math.floor(Math.random() * (1, 100) + 1);
+function playGame() {
+    const args = process.argv.slice(2);
+    // Check for input
+    if (args.length === 0) {
+        console.log("Please provide a guess in the command line!");
+        console.log("Type: node numberGuessingGame.js <your_guess>");
+        console.log("The number is between 1 and 100 (inclusive)");
+    }
+    // Extract input from the array and parse it to an int
+    const input = args[0];
+    const guess = parseInt(input);
+
+    // Validate input
+    if (isNaN(guess)) {
+        console.log("Invalid input. Please enter a number!");
+        return;
+    }
+    if (guess < 1 || guess > 100) {
+        console.log("Your guess must be between 1 and 100!");
+    }
+
+    if (guess === secretNumber) {
+        console.log("Congratulations! You guessed the number!");
+    } else if (guess < secretNumber) {
+        console.log("Too low! Try again.");
+    } else if (guess > secretNumber) {
+        console.log("Too high! Try again.");
+    }
+}
+
+playGame();
