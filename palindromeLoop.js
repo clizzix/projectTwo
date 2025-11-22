fe85c7ae31b9625422d02c4b2a9b8deecf9d238b
clizzix <godsilla1993@gmail.com>
2025-11-22 16:53:19 +0100

    Added description

--- palindromeLoop.js
+++ palindromeLoop.js
@@ -1,8 +1,10 @@
 // Get the users input
 const input = process.argv.slice(2).join("");
 
+// create an empty array to store the reversed input
 let reversedPhrase = [];
 
+// validate input
 if (input === "") {
     console.log("Please enter text.");
 }
