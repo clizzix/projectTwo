501fe5919d9063ce171631eb7517e6630b472e51
clizzix <godsilla1993@gmail.com>
2025-11-29 17:34:43 +0100

    Added changes

--- calculator.js
+++ calculator.js
@@ -6,38 +6,38 @@ let result = 0;
 
 // Check for the correct number of arguments
 if (args.length !== 3) {
-    console.log("Usage: node calculator.js <number1> <operator> <number2>");
-    console.log("Example: node calculator.js 5 + 3");
+    console.log('Usage: node calculator.js <number1> <operator> <number2>');
+    console.log('Example: node calculator.js 5 + 3');
     return;
 }
 
 // Validate operator and Num1 and Num2
 
 if (
-    operator !== "+" &&
-    operator !== "-" &&
-    operator !== "/" &&
-    operator !== "*"
+    operator !== '+' &&
+    operator !== '-' &&
+    operator !== '/' &&
+    operator !== '*'
 ) {
-    console.log("Invalid operator. USe +, -, * or /");
+    console.log('Invalid operator. Use +, -, * or /');
 } else if (isNaN(num1) || isNaN(num2)) {
-    console.log("Invalid input. Please enter a Number.");
+    console.log('Invalid input. Please enter a Number.');
 }
 
 // Calculator Logic
 
-if (operator === "+") {
+if (operator === '+') {
     result = num1 + num2;
     console.log(`Your result is ${result}`);
-} else if (operator === "-") {
+} else if (operator === '-') {
     result = num1 - num2;
     console.log(`Your result is ${result}`);
-} else if (operator === "*") {
+} else if (operator === '*') {
     result = num1 * num2;
     console.log(`Your result is ${result}`);
-} else if (operator === "/") {
-    if (num1 === 0 || num2 === 0) {
-        console.error("You cannot divide by 0!");
+} else if (operator === '/') {
+    if (num2 === 0) {
+        console.error('You cannot divide by 0!');
     } else {
         result = num1 / num2;
         console.log(`Your result is ${result}`);
--- numberGuessingGame.js
+++ numberGuessingGame.js
@@ -5,10 +5,10 @@ console.log(`The random number was ${randomNumber}`);
 function playGame() {
     const args = process.argv.slice(2);
     // Check for input
-    if (args.length === 0) {
-        console.log("Please provide a guess in the command line!");
-        console.log("Type: node numberGuessingGame.js <your_guess>");
-        console.log("The number is between 1 and 10 (inclusive)");
+    if (args.length < 1) {
+        console.log('Please provide a guess in the command line!');
+        console.log('Type: node numberGuessingGame.js <your_guess>');
+        console.log('The number is between 1 and 10 (inclusive)');
     }
     // Extract input from the array and parse it to an int
     const input = args[0];
@@ -16,19 +16,19 @@ function playGame() {
 
     // Validate input
     if (isNaN(guess)) {
-        console.log("Invalid input. Please enter a number!");
+        console.log('Invalid input. Please enter a number!');
         return;
     }
     if (guess < 1 || guess > 10) {
-        console.log("Your guess must be between 1 and 10!");
+        console.log('Your guess must be between 1 and 10!');
     }
 
     if (guess === randomNumber) {
-        console.log("Congratulations! You guessed the number!");
+        console.log('Congratulations! You guessed the number!');
     } else if (guess < randomNumber) {
-        console.log("Too low! Try again.");
+        console.log('Too low! Try again.');
     } else if (guess > randomNumber) {
-        console.log("Too high! Try again.");
+        console.log('Too high! Try again.');
     }
 }
 
